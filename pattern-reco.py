c3bb119731f80722c0a92cdf3958a5974162e90d
david <david@david-HP-Pavilion-g6-Notebook-PC>
2014-02-16 19:53:37 +0100

    Placement des cercles de couleurs OK Mais tres tres lent.

--- pattern-reco.py
+++ pattern-reco.py
@@ -9,6 +9,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.pyplot import cm
 from skimage.color  import rgb2hsv,rgb2lab,hsv2rgb
+from skimage.morphology  import closing,opening,square
+from scipy.ndimage.morphology import distance_transform_edt
+
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.cluster.vq import kmeans,kmeans2,vq
 import math
@@ -18,16 +21,26 @@ class Circle:
         self.center = center
         self.radius = radius
     
+    def __init__(self, center, radius,color):
+        self.center = center
+        self.radius = radius
+        self.color = color
+        
     def area(self):
         return math.pi * self.radius**2
+    
     def contains(self, point):
         dx = point[0] - self.center[0]
         dy = point[1] - self.center[1]
-        return math.sqrt(dx*dx + dy*dy) < self.radius
+        #print ("x courant =",math.sqrt(dx*dx + dy*dy),"Rayon=",self.radius)
+        return (math.sqrt(dx*dx + dy*dy) <= self.radius) 
+    
     def getradius(self):
         return self.radius
     def getcenter(self):
         return self.center
+    def getcolor(self):
+        return self.color
     def setradius(self, r):
         self.radius = r
     def setcenter(self, c):
@@ -72,7 +85,7 @@ def convertLAB(img):
             print ("Image format not supported")
             
 def nbrDiff(img):
-    "count the numbre of different element in an array"
+    "count the number of different element in an array"
     diff = [img[0,0],]
     for i in range(0,img.shape[0]):
         for j in range(0,img.shape[1]):
@@ -139,30 +152,100 @@ def clustering2(img,clusters):
     return clustered
 
 def drawCircle(circle,img,color):
-        for i in range (circle.getcenter[0]-circle.getradius ,circle.getcenter[0]+circle.getradius):
-            for j in range (circle.getcenter[1]-circle.getradius ,circle.getcenter[1]+circle.getradius):
-                if circle.contains ([i,j]):
-                    img[i,j] = color
-        return img            
- 
+    
+    borneInfX= (circle.getcenter()[0]-circle.getradius()).astype(np.int16)
+    borneSupX= (circle.getcenter()[0]+circle.getradius()).astype(np.int16)
+    borneInfY= (circle.getcenter()[1]-circle.getradius()).astype(np.int16)
+    borneSupY=(circle.getcenter()[1]+circle.getradius()).astype(np.int16)
+    #print ("Dans drawCircle", circle.getcenter()[0],circle.getcenter()[1],circle.getradius())
+    #print (borneInfX,borneSupX,borneInfY,borneSupY)
+    if borneInfX < 0:
+        borneInfX =0
+    if borneSupX > img.shape[0]:
+        borneSupX =img.shape[0]
+    if borneInfY < 0:
+        borneInfY =0
+    if borneSupY > img.shape[1]:
+        borneSupY =img.shape[1]
+        
+    for i in range (borneInfX,borneSupX):
+        for j in range (borneInfY,borneSupY):
+            #print ("point dans cercle? ",circle.contains ([i,j]))
+            if circle.contains ([i,j]):
+                img[i,j] = color
+                #print("coloration")
+    return img
+          
+""" 
 def circlePlacementOK(circle,img,color):
     img2 = img
     print circle.getcenter()
     print circle.getradius()
     center = circle.getcenter()
     radius = circle.getradius()
-    if ((center[0]-radius < 0) or (center[0]+radius >img.shape[0]) 
-        or (center[1]-radius < 0) or (center[1]+radius > img.shape[1])):
-        return False;
+
     
     for i in range (center[0]-radius ,center[0]+radius):
             for j in range (center[1]-radius,center[1]+radius):
                 if (circle.contains ([i,j])):
-                    img2 [i,j] = color 
+                    img2 [i,j] = color
     
     count = np.count_nonzero(img -img2)
+    
     return count == 0;
+"""
 
+def circularizationDistMap(img,clusters):
+    go = True
+    listCircle = []
+    threshold = 5
+    for i in range (0,clusters):
+        BinaryImg = (img == i)*1
+        print("nbrDiff(img) = ", nbrDiff(BinaryImg))
+
+        window = plt.figure(2)
+        plt.imshow(BinaryImg,interpolation = 'nearest')
+        plt.title("BinaryImg Avant While")
+        plt.show()
+        go = True
+        while (go):
+            distMap= distance_transform_edt(BinaryImg)
+            """    
+            window = plt.figure(1)
+            plt.imshow(distMap,interpolation = 'nearest')
+            plt.title("distMap dans le While")
+            plt.show()
+            """
+            
+            maxIndex = np.unravel_index(distMap.argmax(), distMap.shape) #recherche de l'indice du maximum de distMap
+            maxTest2 = distMap[maxIndex[0],maxIndex[1]]
+            """
+            maxTest = distMap.max()
+            if maxTest == maxTest2 :
+                print ("Le Max est le bon")
+            """
+            # verification distmap(maxIndex) == distmap.max()
+            circle = Circle ([maxIndex[0],maxIndex[1]],distMap[maxIndex[0],maxIndex[1]],i)
+            BinaryImg = drawCircle(circle,BinaryImg,0)#On place le cercle sur l'img. Les points du cercle appartiennent mnt au fond
+
+            #print ("diff originale - draw",nbrDiff(BinaryImg - tmp))
+            """
+            window = plt.figure(1)
+            plt.imshow(BinaryImg,interpolation = 'nearest')
+            plt.title("BinaryImg Dans While")
+            plt.show()
+            """
+            listCircle.append(circle)#memorize les centres et rayon des cercles
+            #print (listCircle)
+            
+            if distMap[maxIndex[0],maxIndex[1]] < threshold:
+                go = False
+                print("Break du while.")
+                
+            print ('Iteration suivante ',"maxTest = ",maxTest2," maxIndex = ",maxIndex)
+    
+    return listCircle
+    
 def circularization(img):
     "Places the circles"
     diff = nbrDiff(img)
@@ -200,8 +283,24 @@ def circularization(img):
     
     return placement
 
-test= Circle([0,0],1)
-print test.getcenter()
+
+
+def morphoNoiseRemoval(img):
+    imgB = img
+    for i in range(0,5):
+        imgB = opening (imgB, square(3))
+        imgB = closing (imgB, square(3))
+    return imgB
+
+def formationImgCercle (listCircle,img):
+    imgCircle = np.zeros_like(img)
+    print ("nbr cercles = ", len(listCircle))
+    for i in range (0,len(listCircle)):
+        print listCircle[i].getcenter(),listCircle[i].getradius(),listCircle[i].getcolor()
+        imgCircle = drawCircle(listCircle[i],imgCircle,listCircle[i].getcolor()+1)
+    
+    return imgCircle
+
 img = loadImages('false')
 print ("Original Image",img.dtype, type(img),img.shape)
 print ("pixel test Original = ", img[img.shape[0]/2,img.shape[1]/2,:])
@@ -212,33 +311,44 @@ imgHSV = convertHSV(img)
 print ("imgHSV : ", imgHSV.dtype, type(imgHSV),imgHSV.shape)
 print ("pixel test HSV = ", imgHSV[imgHSV.shape[0]/2,imgHSV.shape[1]/2,:])
 
-clusters = 3
+clusters = 4
 imgClus,code = clustering(imgHSV,clusters)
 imgClus = convertHSVtoRGB(imgClus)
+code2 = code.astype(np.uint8)
+morpho = morphoNoiseRemoval(code2)
 print ("Code = ", code.dtype, type(code),code.shape )
-circularization(code)
+lcircle = circularizationDistMap(code2,clusters)
+imgCirc = formationImgCercle (lcircle,code2)
 
-#imgClus2 = convertHSVtoRGB(clustering2(imgHSV,clusters))
 
 """
+#circularization(code)
+#imgClus2 = convertHSVtoRGB(clustering2(imgHSV,clusters))
 kmeanHSV1 = kmeansAlgo(imgHSV)
-
 kmean2 = kmeansAlgo2(img)
 kmeanHSV2 = kmeansAlgo2(imgHSV)
+#imgLAB = convertLAB(img) 
 """
 
-
-
-#imgLAB = convertLAB(img)    
-    
 window1 = plt.figure(1)
-window1.add_subplot(1,2,1)
-plt.title('Original')
-plt.imshow(img)
-window1.add_subplot(1,2,2)
-plt.imshow(imgClus)
-plt.title("After Clustering1")
+plt.title('IMG Cercle')
+plt.imshow(imgCirc,interpolation = 'nearest')
+plt.colorbar()
+plt.show()
 """
+window1 = plt.figure(1)
+plt.title('IMG Originale')
+plt.imshow(img,interpolation = 'nearest')
+window = plt.figure(2)
+plt.imshow(imgClus,interpolation = 'nearest')
+plt.title("Apres Clustering")
+window = plt.figure(3)
+plt.imshow(morpho,interpolation = 'nearest')
+plt.title("Apres operateurs Morpho")
+window = plt.figure(4)
+plt.imshow(code2,interpolation = 'nearest')
+plt.title("Avant operateurs Morpho")
+plt.show()
 window2= plt.figure(2)
 plt.imshow(imgClus2)
 plt.title("After Clustering2")
